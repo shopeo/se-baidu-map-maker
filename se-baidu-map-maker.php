55a441691978726e5f92917b4bbede098cb02c33
jabber <2898117012@qq.com>
2023-07-24 20:34:02 +0800

    fix: init

---
+++ se-baidu-map-maker.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name: SE Baidu Map Maker
+ * Plugin URI: https://wordpress.org/plugins/se-baidu-map-maker
+ * Description: SHOPEO Baidu Map Maker
+ * Author: Shopeo
+ * Version: 0.0.1
+ * Author URI: https://shopeo.cn
+ * License: GPL2+
+ * Text Domain: se-baidu-map-maker
+ * Domain Path: /languages
+ * Requires at least: 5.9
+ * Requires PHP: 5.6
+ */
+
+require_once 'vendor/autoload.php';
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
