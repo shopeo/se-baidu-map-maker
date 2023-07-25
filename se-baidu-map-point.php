28c0fc4ebf10f4330371a3879a1a78b30fb1131c
jabber <2898117012@qq.com>
2023-07-25 21:24:44 +0800

    init

--- se-baidu-map-point.php
+++ se-baidu-map-point.php
@@ -13,9 +13,47 @@
  * Requires PHP: 5.6
  */
 
+use src\SEBaiduMapCore;
+
 require_once 'vendor/autoload.php';
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
+if ( ! defined( 'SE_BAIDU_MAP_POINT_FILE' ) ) {
+	define( 'SE_BAIDU_MAP_POINT_FILE', __FILE__ );
+}
+
+if ( ! function_exists( 'se_baidu_map_point_activation' ) ) {
+	function se_baidu_map_point_activation() {
+
+	}
+}
+
+register_activation_hook( __FILE__, 'se_baidu_map_point_activation' );
+
+if ( ! function_exists( 'se_baidu_map_point_deactivation' ) ) {
+	function se_baidu_map_point_deactivation() {
+
+	}
+}
+
+register_deactivation_hook( __FILE__, 'se_baidu_map_point_deactivation' );
+
+add_action( 'init', function () {
+	load_plugin_textdomain( 'se-baidu-map-point', false, dirname( __FILE__ ) . '/languages' );
+	if ( ! defined( 'SE_Baidu_Map_Core_Loaded' ) ) {
+		new SEBaiduMapCore();
+	}
+} );
+
+$plugin_version = get_plugin_data( SE_BAIDU_MAP_POINT_FILE )['Version'];
+
+add_action( 'admin_enqueue_scripts', function () {
+
+} );
+
+add_action( 'wp_enqueue_scripts', function () {
+
+} );
